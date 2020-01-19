14dc1a6f3a9695053840addf85b2ca957a4e1976
rogergale <rgagale@hotmail.com>
2020-01-19 22:07:50 +0000

    Initial project setup from Spring Initializr

---
+++ geographic-search-service/src/main/java/com/dwp/gis/search/GeographicSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dwp.gis.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeographicSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeographicSearchServiceApplication.class, args);
+	}
+
+}
---
+++ geographic-search-service/src/test/java/com/dwp/gis/search/GeographicSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwp.gis.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeographicSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
